ad42d2cf4fd1a3e5fa9432342cf4ea1fc191e411
GsCruz0 <202411240014@ifba.edu.br>
2026-04-22 15:53:40 -0300

    BoletoBradescoBuilder.java

--- BoletoBradescoBuilder.java
+++ BoletoBradescoBuilder.java
@@ -1,22 +1,27 @@
 public class BoletoBradescoBuilder extends BoletoBuilder {
 
+    public void buildBeneficiario() {
+        boleto.nomeBeneficiario = "Empresa Bradesco";
+        boleto.cpfCnpjBeneficiario = "11.111.111/0001-11";
+        boleto.enderecoBeneficiario = "SP";
+    }
+
+    public void buildSacado() {
+        boleto.nomeSacado = "Maria Souza";
+        boleto.cpfCnpjSacado = "222.222.222-22";
+        boleto.enderecoSacado = "RJ";
+    }
+
+    public void buildTitulo() {
+        boleto.numeroDocumento = "456";
+        boleto.dataVencimento = "25/04/2026";
+        boleto.valor = 200.00;
+    }
+
     public void buildBanco() {
         boleto.banco = "Bradesco";
-    }
-
-    public void buildValor() {
-        boleto.valor = 300.50;
-    }
-
-    public void buildVencimento() {
-        boleto.vencimento = "30/04/2026";
-    }
-
-    public void buildPagador() {
-        boleto.pagador = "Carlos Lima";
-    }
-
-    public void buildBeneficiario() {
-        boleto.beneficiario = "Empresa 123";
+        boleto.agencia = "2222";
+        boleto.conta = "33333";
+        boleto.carteira = "06";
     }
 }
