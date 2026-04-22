0ffeb64781ac1bdcf07593444c0cc1df95116c4e
GsCruz0 <202411240014@ifba.edu.br>
2026-04-22 15:52:34 -0300

    Boleto.java

--- Boleto.java
+++ Boleto.java
@@ -1,16 +1,32 @@
 public class Boleto {
-    String banco;
+    String nomeBeneficiario;
+    String cpfCnpjBeneficiario;
+    String enderecoBeneficiario;
+
+    String nomeSacado;
+    String cpfCnpjSacado;
+    String enderecoSacado;
+
+    String numeroDocumento;
+    String dataVencimento;
     double valor;
-    String vencimento;
-    String pagador;
-    String beneficiario;
+
+    String banco;
+    String agencia;
+    String conta;
+    String carteira;
+
+    String codigoBarras;
+    String linhaDigitavel;
 
     public void mostrarBoleto() {
         System.out.println("Banco: " + banco);
+        System.out.println("Beneficiário: " + nomeBeneficiario);
+        System.out.println("Sacado: " + nomeSacado);
         System.out.println("Valor: R$ " + valor);
-        System.out.println("Vencimento: " + vencimento);
-        System.out.println("Pagador: " + pagador);
-        System.out.println("Beneficiário: " + beneficiario);
+        System.out.println("Vencimento: " + dataVencimento);
+        System.out.println("Linha Digitável: " + linhaDigitavel);
+        System.out.println("Código de Barras: " + codigoBarras);
         System.out.println("---------------------------");
     }
 }
