533509932fff7f809c4510b982f4694bae1cc9a9
GsCruz0 <202411240014@ifba.edu.br>
2026-04-22 15:53:18 -0300

    BoletoItauBuilder.java

--- BoletoItauBuilder.java
+++ BoletoItauBuilder.java
@@ -1,22 +1,27 @@
 public class BoletoItauBuilder extends BoletoBuilder {
 
-    public void buildBanco() {
-        boleto.banco = "Itaú";
+    public void buildBeneficiario() {
+        boleto.nomeBeneficiario = "Empresa Itaú";
+        boleto.cpfCnpjBeneficiario = "00.000.000/0001-00";
+        boleto.enderecoBeneficiario = "São Paulo";
     }
 
-    public void buildValor() {
+    public void buildSacado() {
+        boleto.nomeSacado = "João Silva";
+        boleto.cpfCnpjSacado = "111.111.111-11";
+        boleto.enderecoSacado = "SP";
+    }
+
+    public void buildTitulo() {
+        boleto.numeroDocumento = "123";
+        boleto.dataVencimento = "20/04/2026";
         boleto.valor = 150.75;
     }
 
-    public void buildVencimento() {
-        boleto.vencimento = "20/04/2026";
-    }
-
-    public void buildPagador() {
-        boleto.pagador = "João Silva";
-    }
-
-    public void buildBeneficiario() {
-        boleto.beneficiario = "Empresa XYZ";
+    public void buildBanco() {
+        boleto.banco = "Itaú";
+        boleto.agencia = "1234";
+        boleto.conta = "56789";
+        boleto.carteira = "109";
     }
 }
