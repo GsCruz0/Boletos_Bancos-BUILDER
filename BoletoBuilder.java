0fff17413122fa6513bbe06b2a0392b293f64837
GsCruz0 <202411240014@ifba.edu.br>
2026-04-22 15:52:57 -0300

    BoletoBuilder.java

--- BoletoBuilder.java
+++ BoletoBuilder.java
@@ -5,11 +5,15 @@ public abstract class BoletoBuilder {
         boleto = new Boleto();
     }
 
-    public abstract void buildBanco();
-    public abstract void buildValor();
-    public abstract void buildVencimento();
-    public abstract void buildPagador();
     public abstract void buildBeneficiario();
+    public abstract void buildSacado();
+    public abstract void buildTitulo();
+    public abstract void buildBanco();
+
+    public void gerarCodigoBarrasELinha() {
+        boleto.codigoBarras = "12345678901234567890123456789012345678901234";
+        boleto.linhaDigitavel = "12345.67890 12345.678901 12345.678901 1 12345678901234";
+    }
 
     public Boleto getBoleto() {
         return boleto;
